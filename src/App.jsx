e387b61e7a92f5e4dec43218479c08f54972ccd3
unknown <jagadeeshganta7@gmail.com>
2025-04-01 06:59:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  let name1 ='jhone';
+  return (
+    <div>
+      <h1 className=''>this is heading</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, minima!</p>
+      <p>{2 + 4}</p>
+      <p>2+4</p>
+      <p>{name1}</p>
+    </div>
+  )
+}
+
+// const sum = () =>{
+
+// }
+
+
+export default App;
